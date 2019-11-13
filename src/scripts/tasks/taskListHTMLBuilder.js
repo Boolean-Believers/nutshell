961714f8473063f382461ce5a6d5877fb3a200b7
rhc822 <rhc822@gmail.com>
2019-11-13 10:28:17 -0600

    Created Task List HTML form

---
+++ src/scripts/tasks/taskListHTMLBuilder.js
@@ -0,0 +1,12 @@
+taskListHTMLBuilder(task) {
+    return `
+    <section id="taskID--${task.id}">
+        <label for="completedTask">Mark Complete</label>
+        <input id="completedTask--${task.id}" type="checkbox">
+        <h1>${task.taskName}</h1>
+        <h2>${task.completionDate}</h2>
+        <button id="taskEditButton--${task.id}">EDIT</button>
+        <button id="taskDeleteButton--${task.id}">DELETE</button>
+    </section>
+    `;
+  }
