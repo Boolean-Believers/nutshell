d1257984d347842348f028da2f07cba747fdcc9d
Melody Stern <melliemuse@gmail.com>
2019-11-14 11:13:20 -0600

    added reference to messages section container

--- src/scripts/messages/eventListeners.js
+++ src/scripts/messages/eventListeners.js
@@ -1,5 +1,6 @@
 const sendNewMessageButton = document.querySelector("#submitNewMessage-button")
 const editMessageButton = document.querySelector("#editMessage-button")
+const messagesContainer = document.querySelector("#messages-section")
 
 const eventListeners = {
 }
